7cce3044143b0c76a25f289766218dd2f532dbd4
William Aguas <148153196+WILLIAMAGUAS@users.noreply.github.com>
2025-04-29 13:51:40 +0000

    proyecto terminado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,17 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [ color, setColor ] = useState("green");
 	return (
 		<div className="text-center d-flex justify-content-center flex-column align-items-center">
 			<div className="palito"></div>
             <div className="semaforo">
-				<div className="luz bg-danger "> </div>
-				<div className="luz bg-warning brillo"> </div>
-				<div className="luz bg-success "> </div>
+				<div className ={`luz bg-danger ${color==="red" ? "brillo" : ""}`} onClick={()=>{setColor("red")}}> </div>
+				<div className={`luz bg-warning ${color==="yellow" ? "brillo" : ""}`} onClick={()=>{setColor("yellow")}}> </div>
+				<div className={`luz bg-success ${color==="green" ? "brillo" : ""}`} onClick={()=>{setColor("green")}}> </div>
 
 			</div>
 
